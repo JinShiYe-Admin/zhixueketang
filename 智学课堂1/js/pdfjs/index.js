f60408991ece19aacb3eb8f85fa7ca6e4b546bfc
Zqw <578159259@qq.com>
2019-01-23 09:36:41 +0800

    修改显示

--- 智学课堂1/js/pdfjs/index.js
+++ 智学课堂1/js/pdfjs/index.js
@@ -5,32 +5,35 @@ PDFJS.cMapPacked = true;
 
 var pdfDoc = null;
 
-function createPage() {
-    var div = document.createElement("canvas");
-    document.body.appendChild(div);
-    return div;
+function createPage(num) {
+	var div = document.createElement("canvas");
+	if(num == 1) {
+		div.style.marginTop = '44px';
+	}
+	document.body.appendChild(div);
+	return div;
 }
 
 function renderPage(num) {
-    pdfDoc.getPage(num).then(function (page) {
-        var viewport = page.getViewport(2.0);
-        var canvas = createPage();
-        var ctx = canvas.getContext('2d');
+	pdfDoc.getPage(num).then(function(page) {
+		var viewport = page.getViewport(2.0);
+		var canvas = createPage(num);
+		var ctx = canvas.getContext('2d');
 
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
+		canvas.height = viewport.height;
+		canvas.width = viewport.width;
 
-        page.render({
-            canvasContext: ctx,
-            viewport: viewport
-        });
-    });
+		page.render({
+			canvasContext: ctx,
+			viewport: viewport
+		});
+	});
 }
 
-PDFJS.getDocument(url).then(function (pdf) {
+PDFJS.getDocument(url).then(function(pdf) {
 	events.closeWaiting();
-    pdfDoc = pdf;
-    for (var i = 1; i <= pdfDoc.numPages; i++) {
-        renderPage(i)
-    }
+	pdfDoc = pdf;
+	for(var i = 1; i <= pdfDoc.numPages; i++) {
+		renderPage(i)
+	}
 });
