a3f4d07202feeeb1db861e3c8afcfdffc23c3b8e
liuxiaoshuai <721685743@qq.com>
2019-02-25 15:14:32 +0800

    使用原生打开在线文档

--- 智学课堂1/js/utils/pushPermission.js
+++ 智学课堂1/js/utils/pushPermission.js
@@ -1,10 +1,10 @@
 ﻿document.addEventListener("plusready", function() {
-	var _BARCODE = 'pushPermission',
+	var _BARCODE = 'OpenFile',
 		B = window.plus.bridge;
 	
 	var pushPermission = {
 		//检测权限是否开启
-		isNotificationEnabled: function(successCallback, errorCallback) {
+		openFileFromURL: function(url,successCallback, errorCallback) {
 			var success = typeof successCallback !== 'function' ? null : function(args) {
 					successCallback(args);
 				},
@@ -12,20 +12,8 @@
 					errorCallback(code);
 				};
 			callbackID = B.callbackId(success, fail);
-			return B.exec(_BARCODE,'isNotificationEnabled', [callbackID]);
+			return B.exec(_BARCODE,'openFileFromURL', [callbackID,url]);
 		},
-		//打开通知权限
-		gotoSetting: function() {
-			var success = typeof successCallback !== 'function' ? null : function(args) {
-					console.log("result>>>>>>"+args);
-				},
-				fail = typeof errorCallback !== 'function' ? null : function(code) {
-					console.log("result>>>>>>"+code);
-				};
-			callbackID = B.callbackId(success, fail);
-			return B.exec(_BARCODE, "gotoSetting", [callbackID]);
-		}
-		
 	};
 	window.plus.pushPermission = pushPermission;
 }, true);
